111b1b2f07f28893f5f5cf0357107efe0ca0af69
samojeyinka <samuelojeyinka@gmail.com>
2024-03-29 06:55:35 -1200

    added favicon to the welcome page

--- welcome.php
+++ welcome.php
@@ -5,7 +5,8 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="style.css"
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
 </head>
 
 <body>
